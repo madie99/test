ccfa481d567501ea4285449a4f2cae548eeca381
madie99 <pmbhavaghni@gmail.com>
2024-04-10 11:24:30 +0530

    added calculator class

---
+++ calculator.java
@@ -0,0 +1,10 @@
+public class calculator {
+    int x;
+    int add_me_to_x;
+    public calculator(int x,int add_me_to_x){
+        this.x=x;
+        this.add_me_to_x=add_me_to_x;
+
+    }
+    
+}
