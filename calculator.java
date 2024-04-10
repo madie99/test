711a0681ee711baa35648065f281c998771bd405
madie99 <pmbhavaghni@gmail.com>
2024-04-10 12:30:06 +0530

    added add func

--- calculator.java
+++ calculator.java
@@ -6,6 +6,9 @@ public class calculator {
         this.add_me_to_x=add_me_to_x;
 
     }
+    public int add(){
+        return x+add_me_to_x;
+    }
     public int sub (int z){
         return x-z;
     }
